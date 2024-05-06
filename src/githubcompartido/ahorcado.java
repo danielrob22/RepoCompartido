b6955e9ac5b1b36da33b05b3a0716ee822b9e3d1
danielrob22 <daniel.robcam@educa.jcyl.es>
2024-05-06 15:47:14 +0200

    prueba subir

--- src/githubcompartido/ahorcado.java
+++ src/githubcompartido/ahorcado.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class ahorcado {
-
+//Prueba
     public static void main(String[] args) {
         //Se declaran variables
         Scanner in = new Scanner(System.in);
